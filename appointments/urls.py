801c89fb021f30a6d1f754f9110b12c377337629
goutham2027 <goutham2027@gmail.com>
2016-05-19 15:57:43 +0530

    - fix datetimepicker format, fix urls

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views.patient_views import PatientsListView, PatientView
 from .views.physician_views import PhysiciansListView, PhysicianView
 from .views.appointment_views import AppointmentsListView, AppointmentView, MainView
-from . import views
+from .views.user_views import login, auth_view, logout, loggedin, invalid_login
 
 app_name = 'appointments'
 urlpatterns = [
@@ -18,10 +18,10 @@ urlpatterns = [
         url(r'^appointments/$', AppointmentsListView.as_view(), name='appointment_list'),
         url(r'^new_appointment/$', AppointmentView.as_view(), name='appointment_new'),
 
-        url(r'^login/$',  views.login, name='login'),
-        url(r'^auth/$',  views.auth_view, name='auth_view'),
-        url(r'^logout/$', views.logout, name='logout'),
-        url(r'^loggedin/$', views.loggedin, name='loggedin'),
-        url(r'^invalid/$', views.invalid_login, name='invalid_login'),
+        url(r'^login/$',  login, name='login'),
+        url(r'^auth/$',  auth_view, name='auth_view'),
+        url(r'^logout/$', logout, name='logout'),
+        url(r'^loggedin/$', loggedin, name='loggedin'),
+        url(r'^invalid/$', invalid_login, name='invalid_login'),
 
 ]
